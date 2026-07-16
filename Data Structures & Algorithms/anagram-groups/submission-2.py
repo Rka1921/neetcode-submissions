17bb61339cf0fab612ae4122c0037ab77669ab0e
Rka1921 <77318837+Rka1921@users.noreply.github.com>
2026-07-16 15:36:13 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d={}
+        for i in strs:
+            key= "".join(sorted(i))
+            if key not in d:
+                d[key] = []
+            d[key].append(i)
+        return(list(d.values()))
+        
